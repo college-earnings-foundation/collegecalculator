d2f568cd11c6ef606911609cdc63a73f378814e0
Chris Cruz <christopherc1331@gmail.com>
2019-06-24 20:47:24 -0700

    Corrected class (JS) structure

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 console.log("index.js is linked to the index.html file");
 
 class TabCard {
-  constructor(element) {}
+  constructor(card) {
+    this.card = card;
+  }
 }
 
 allTabs = document
-  .querySelectorAll(".cta-text")
+  .querySelectorAll(".cta-card")
   .forEach(item => new TabCard(item));
