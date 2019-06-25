ff795b056d56655e919f391e0eda7de2d916065e
Chris Cruz <christopherc1331@gmail.com>
2019-06-24 21:29:35 -0700

    Added defer='defer' to script link in head of index.html to get scripts to run

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,11 @@ console.log("index.js is linked to the index.html file");
 class TabCard {
   constructor(card) {
     this.card = card;
+    console.log(this);
+    this.fullParagraph = card.querySelector("p");
+    console.log(this.fullParagraph);
+    // this.charLimitParagraph = fullParagraph.substring(0, 100);
+    // console.log(this.charLimitParagraph);
   }
 }
 
