7fa78e4738fbec65d6ddf8aeedefee109fccbf81
Chris Cruz <christopherc1331@gmail.com>
2019-06-24 20:44:21 -0700

    Added hidden-text class display none, and basic class JS structure to be filled in

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
 console.log("index.js is linked to the index.html file");
+
+class TabCard {
+  constructor(element) {}
+}
+
+allTabs = document
+  .querySelectorAll(".cta-text")
+  .forEach(item => new TabCard(item));
