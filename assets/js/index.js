dce7a48f18cf696a030f8cec836e8683e4723a08
Chris Cruz <christopherc1331@gmail.com>
2019-06-25 18:51:06 -0700

    Finished expand buttons on home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,20 +3,33 @@ console.log("index.js is linked to the index.html file");
 class TabCard {
   constructor(card) {
     this.card = card;
-    this.fullParagraphArr = [];
-    this.fullParagraph = card.querySelector("p");
-    this.fullParagraphArr.push(this.fullParagraph);
-    console.log(this.fullParagraphArr);
-    this.charLimitParagraph = this.fullParagraph.textContent.substring(0, 100);
-    this.displayedText = this.charLimitParagraph + "...";
-    this.adjustedParagraph = card.querySelector("p");
-    this.adjustedParagraph.textContent = this.displayedText;
-    console.log(this.adjustedParagraph);
-    this.card.addEventListener("click", event => {
+    this.displayedParagraph = card.querySelector("p");
+    //Initialize DOM "p" elements for future use
+    this.fullParagraph = document.createElement("p");
+    this.fullParagraph.textContent = this.displayedParagraph.textContent;
+    //Create new Full Paragraph element to be toggled
+    this.charLimitParagraph = document.createElement("p");
+    this.charLimitParagraph.textContent =
+      this.displayedParagraph.textContent.substring(0, 100) + "...";
+    //Create new char limited Paragraph element with added dots to be toggled
+    this.displayedParagraph.textContent = this.charLimitParagraph.textContent;
+    //Set displayed paragraph element to be limited char len
+    this.cardButton = card.querySelector("button");
+    this.cardButton.addEventListener("click", event => {
       this.changeContent();
     });
   }
-  changeContent() {}
+  changeContent() {
+    //On click, if formula will check which (limited or not) var is currently displayed and will toggle
+    if (
+      this.displayedParagraph.textContent ===
+      this.charLimitParagraph.textContent
+    ) {
+      this.displayedParagraph.textContent = this.fullParagraph.textContent;
+    } else {
+      this.displayedParagraph.textContent = this.charLimitParagraph.textContent;
+    }
+  }
 }
 
 allTabs = document
