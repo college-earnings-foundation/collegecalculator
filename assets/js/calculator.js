a2f04a35c3e3dcfa17c3947f45e60851c45cdf6d
Chris Cruz <christopherc1331@gmail.com>
2019-06-25 20:23:26 -0700

    Attempted to set click handler for calc page; still working on it

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,1 +1,15 @@
 console.log("calculator.js is linked to the index.html file");
+
+class TabCard {
+  constructor(card) {
+    this.card = card;
+    this.titleImage = card.querySelector(".titleImage");
+    this.input = card.querySelector(".input-note");
+    this.card.addEventListener("click", event => {
+      card.toggle();
+    });
+  }
+  toggle() {}
+}
+
+document.querySelectorAll(".cta-card").forEach(item => new TabCard(item));
