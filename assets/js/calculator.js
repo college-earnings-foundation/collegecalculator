1b79f9f0aca749358e2de5e6a553520e148e9d22
Chris Cruz <christopherc1331@gmail.com>
2019-06-26 20:46:51 -0700

    Got 50% of click handler complete

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -5,11 +5,17 @@ class TabCard {
     this.card = card;
     this.titleImage = card.querySelector(".titleImage");
     this.input = card.querySelector(".input-note");
-    this.card.addEventListener("click", event => {
-      card.toggle();
+    this.titleImage.addEventListener("click", event => {
+      this.toggle();
     });
   }
-  toggle() {}
+  toggle() {
+    console.log("titleImage was clicked!");
+    this.titleImage.classList.remove("titleImage");
+    this.titleImage.classList.add("toggle-title");
+    this.input.classList.remove("input-note");
+    this.input.classList.add("toggle-input");
+  }
 }
 
 document.querySelectorAll(".cta-card").forEach(item => new TabCard(item));
