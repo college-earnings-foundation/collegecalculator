ec8126a7020620732358bfbab2a1b5413586f779
Chris Cruz <christopherc1331@gmail.com>
2019-06-26 21:51:04 -0700

    Finished JS functionality for input cost per card toggling styles; still need to find way to save values entered, but will do that later

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -5,16 +5,29 @@ class TabCard {
     this.card = card;
     this.titleImage = card.querySelector(".titleImage");
     this.input = card.querySelector(".input-note");
+    this.inputBox = card.querySelector("input");
     this.titleImage.addEventListener("click", event => {
       this.toggle();
     });
+
+    this.inputBox.addEventListener("keyup", event => {
+      if (event.keycode === 13 || event.which === 13) {
+        this.enterText();
+      }
+    });
   }
+
   toggle() {
-    console.log("titleImage was clicked!");
     this.titleImage.classList.remove("titleImage");
     this.titleImage.classList.add("toggle-title");
     this.input.classList.remove("input-note");
     this.input.classList.add("toggle-input");
+  }
+  enterText() {
+    this.titleImage.classList.add("titleImage");
+    this.titleImage.classList.remove("toggle-title");
+    this.input.classList.add("input-note");
+    this.input.classList.remove("toggle-input");
   }
 }
 
