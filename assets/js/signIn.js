70ccd0235d51fae9d7e00c75fdafc2ba64fd427f
calebredd <caleb.redd@yahoo.com>
2019-06-27 20:58:27 -0700

    Styled sign in button to toggle cancel when open

--- assets/js/signIn.js
+++ assets/js/signIn.js
@@ -7,19 +7,27 @@ var username = document.querySelector(".username");
 var welcome = document.querySelector(".welcome");
 var credentials = document.querySelector(".credentials");
 signin.addEventListener("click", function() {
- 
-  credentials.classList.remove("hidden");
+  check();
+  credentials.classList.toggle("hidden");
   welcome.classList.remove("hidden");
   welcome.innerHTML = "Welcome, please Sign In!";
-
 });
-login.addEventListener("click", function () {
-  welcome.innerHTML = "Welcome to College Calculator " + username.value+"!";
+login.addEventListener("click", function() {
+  welcome.innerHTML = "Welcome to College Calculator " + username.value + "!";
   welcome.classList.remove("hidden");
   credentials.classList.add("hidden");
+  check();
 });
 
-submit.addEventListener("click",function(){
+submit.addEventListener("click", function() {
   after.classList.remove("hidden");
   during.classList.add("hidden");
 });
+
+check = function () {
+  if (signin.innerHTML === "Sign In") {
+    signin.innerHTML = "Cancel";
+    console.log(signin);
+  } else {
+    signin.innerHTML = "Sign In";
+  }};
