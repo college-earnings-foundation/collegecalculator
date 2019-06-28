6c6edf5b6f838a6db45877c8b7987531d7996a76
calebredd <caleb.redd@yahoo.com>
2019-06-27 20:29:49 -0700

    Still working on contact page hidden features

--- assets/js/signIn.js
+++ assets/js/signIn.js
@@ -1,5 +1,7 @@
 var signin = document.querySelector(".signin");
-
+var submit = document.querySelector("#submit_button");
+var after = document.querySelector("#after");
+var during = document.querySelector("#during");
 var login = document.querySelector(".login");
 var username = document.querySelector(".username");
 var welcome = document.querySelector(".welcome");
@@ -15,4 +17,9 @@ login.addEventListener("click", function () {
   welcome.innerHTML = "Welcome to College Calculator " + username.value+"!";
   welcome.classList.remove("hidden");
   credentials.classList.add("hidden");
+});
+
+submit.addEventListener("click",function(){
+  after.classList.remove("hidden");
+  during.classList.add("hidden");
 });
